4fe4b36fcc32a32991b31a1bd9ca3aa1297b761e
Nitin Mishra <mnitin2311@gmail.com>
2020-11-01 17:01:46 +0530

    See Profile of individual users

--- profiles/models.py
+++ profiles/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 from .utils import get_random_code
 from django.template.defaultfilters import slugify
 from django.db.models import Q
-
-# models here.
+# Create your models here.
 
 class ProfileManager(models.Manager):
+
     def get_all_profiles_to_invite(self, sender):
         profiles = Profile.objects.all().exclude(user=sender)
         profile = Profile.objects.get(user=sender)
@@ -32,26 +32,32 @@ class ProfileManager(models.Manager):
     def get_all_profiles(self, me):
         profiles = Profile.objects.all().exclude(user=me)
         return profiles
-    
+
 
 class Profile(models.Model):
-    first_name = models.CharField(max_length=200,blank = True)
-    last_name = models.CharField(max_length=100,blank=True)
-    user = models.OneToOneField(User ,on_delete=models.CASCADE)
-    bio = models.TextField(default="User has no Bio!",max_length =500)
-    email = models.EmailField(max_length= 200,blank = True)
-    country = models.CharField(max_length= 200,blank = True)
-    avatar = models.ImageField(default='avatar.png', upload_to ='avatars/')
-    friends = models.ManyToManyField(User , blank=True , related_name ='friends')
-    slug = models.SlugField(unique=True , blank=True)
+    first_name = models.CharField(max_length=200, blank=True)
+    last_name = models.CharField(max_length=200, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(default="no bio...", max_length=300)
+    email = models.EmailField(max_length=200, blank=True)
+    country = models.CharField(max_length=200, blank=True)
+    avatar = models.ImageField(default='avatar.png', upload_to='avatars/')
+    friends = models.ManyToManyField(User, blank=True, related_name='friends')
+    slug = models.SlugField(unique=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     objects = ProfileManager()
 
+    def __str__(self):
+        return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')}"
+
+    def get_absolute_url(self):
+        return reverse("profiles:profile-detail-view", kwargs={"slug": self.slug})
+    
     def get_friends(self):
         return self.friends.all()
-    
+
     def get_friends_no(self):
         return self.friends.all().count()
 
@@ -61,7 +67,7 @@ class Profile(models.Model):
     def get_all_authors_posts(self):
         return self.posts.all()
 
-    def get_likes_given(self):
+    def get_likes_given_no(self):
         likes = self.like_set.all()
         total_liked = 0
         for item in likes:
@@ -70,43 +76,51 @@ class Profile(models.Model):
         return total_liked
 
     def get_likes_recieved_no(self):
-        posts=self.posts.all()
-        total_liked=0
+        posts = self.posts.all()
+        total_liked = 0
         for item in posts:
-            total_liked +=item.liked.all().count()
+            total_liked += item.liked.all().count()
         return total_liked
- 
-    def __str__(self):
-        return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')}"
 
-    def save(self,*args,**kwargs):
-        ex=False
-        if self.first_name and self.last_name: 
-            to_slug = slugify(str(self.first_name) + " " + str(self.last_name))
-            ex = Profile.objects.filter(slug=to_slug).exists()
-            while ex:
-                to_slug = slugify(to_slug + " " + str(get_random_code()))
+
+    __initial_first_name = None
+    __initial_last_name = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__initial_first_name = self.first_name
+        self.__initial_last_name = self.last_name
+
+    def save(self, *args, **kwargs):
+        ex = False
+        to_slug = self.slug
+        if self.first_name != self.__initial_first_name or self.last_name != self.__initial_last_name or self.slug=="":
+            if self.first_name and self.last_name:
+                to_slug = slugify(str(self.first_name) + " " + str(self.last_name))
                 ex = Profile.objects.filter(slug=to_slug).exists()
-        else:
-             to_slug = str(self.user)
+                while ex:
+                    to_slug = slugify(to_slug + " " + str(get_random_code()))
+                    ex = Profile.objects.filter(slug=to_slug).exists()
+            else:
+                to_slug = str(self.user)
         self.slug = to_slug
         super().save(*args, **kwargs)
 
-
-STATUS_CHOICES=(
-    ('send','send'),
-    ('accepted','accepted')
+STATUS_CHOICES = (
+    ('send', 'send'),
+    ('accepted', 'accepted')
 )
 
 class RelationshipManager(models.Manager):
-    def invitations_received(self,receiver):
-        qs = Relationship.objects.filter(receiver=receiver, status= 'send')
+    def invatations_received(self, receiver):
+        qs = Relationship.objects.filter(receiver=receiver, status='send')
         return qs
-    
+
+
 class Relationship(models.Model):
-    sender = models.ForeignKey(Profile , on_delete=models.CASCADE, related_name='sender')
-    receiver = models.ForeignKey(Profile , on_delete=models.CASCADE, related_name='receiver')
-    status = models.CharField(max_length=9 ,choices=STATUS_CHOICES)
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
@@ -114,3 +128,8 @@ class Relationship(models.Model):
 
     def __str__(self):
         return f"{self.sender}-{self.receiver}-{self.status}"
+        
+
+    
+        
+
